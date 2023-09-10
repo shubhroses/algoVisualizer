fecb026573759970ce0f1c27e249cce326c61a40
Shubhrose Singh <shubhroses@gmail.com>
2023-09-10 11:35:30 -0500

    First commit

---
+++ sorting_algorithms/bubble_sort.py
@@ -0,0 +1,14 @@
+def bubble_sort(arr):
+    steps = []
+    n = len(arr)
+    for i in range(n):
+        for j in range(n-i-1):
+            steps.append(arr.copy())
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    steps.append(arr.copy())  # Capture the final sorted array
+    return arr, steps
+
+steps = []
+arr = [4, 2, 7, 1, 9]
+bubble_sort(arr)
